a6cc2fbc776704f14fcd0d57fd21a78517f4cffd
vath <bro.vath7547@gmail.com>
2025-01-14 15:36:56 +0700

    Task01

---
+++ src/subject.java
@@ -0,0 +1,37 @@
+public class subject {
+
+    private static final double price;
+    static {
+        price = 9.9;
+    }
+
+    private static final String PROGRAM = "Year 2 Program";
+
+    private String name;
+    private int credits;
+
+    public static final double getPrice() {
+        return price;
+    }
+
+    public static String getProgram() {
+        return PROGRAM;
+    }
+
+    public subject(String name, int credits) {
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
